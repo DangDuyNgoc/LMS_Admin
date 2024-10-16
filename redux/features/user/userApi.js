1b3187faaf005b071308b9fa99c58aac536e40a2
hackTran <ductv.21it@vku.udn.vn>
2024-10-16 15:13:56 +0700

    delete user

--- redux/features/user/userApi.js
+++ redux/features/user/userApi.js
@@ -11,28 +11,43 @@ export const userApi = apiSlice.injectEndpoints({
       }),
     }),
     editProfile: builder.mutation({
-        query: ({ name }) => ({
-          url: "user/update-user",
-          method: "PUT",
-          body: { name },
-          credentials: "include",
-        }),
+      query: ({ name }) => ({
+        url: "user/update-user",
+        method: "PUT",
+        body: { name },
+        credentials: "include",
       }),
+    }),
     updateUserPassword: builder.mutation({
-        query: (oldPassword, newPassword) => ({
-          url: "user/update-password",
-          method: "PUT",
-          body: { oldPassword, newPassword },
-          credentials: "include",
-        }),
+      query: (oldPassword, newPassword) => ({
+        url: "user/update-password",
+        method: "PUT",
+        body: { oldPassword, newPassword },
+        credentials: "include",
       }),
-      getAllUsers: builder.query({
-        query: () => ({
-          url: "user/get-users",
-          method: "GET",
-          credentials: "include",
-        }),
+    }),
+    getAllUsers: builder.query({
+      query: () => ({
+        url: "user/get-users",
+        method: "GET",
+        credentials: "include",
       }),
+    }),
+    updateUserRole: builder.mutation({
+      query: ({ email, role }) => ({
+        url: "user/update-user-role",
+        method: "PUT",
+        body: { email, role },
+        credentials: "include",
+      }),
+    }),
+    deleteUser: builder.mutation({
+      query: (id) => ({
+        url: `user/delete-user/${id}`,
+        method: "DELETE",
+        credentials: "include",
+      }),
+    }),
   }),
 });
 
@@ -41,4 +56,6 @@ export const {
   useEditProfileMutation,
   useUpdateUserPasswordMutation,
   useGetAllUsersQuery,
+  useUpdateUserRoleMutation,
+  useDeleteUserMutation,
 } = userApi;
